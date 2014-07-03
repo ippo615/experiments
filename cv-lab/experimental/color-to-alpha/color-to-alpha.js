7630d6eddae3468d8f129a4b395c3d0f93d3af42
Andrew Ippoliti <acippolit@gmail.com>
2014-07-02 21:55:53 -0400

    color-to-alpha matches new api

--- cv-lab/experimental/color-to-alpha/color-to-alpha.js
+++ cv-lab/experimental/color-to-alpha/color-to-alpha.js
@@ -8,22 +8,25 @@ function colorDist( px1, px2 ){
 	return Math.sqrt( delta[0]*delta[0] + delta[1]*delta[1] + delta[2]*delta[2] );
 }
 
-function colorToAlpha( src, dst, color ) {
+function colorToAlpha( src, color ) {
 
   var xSize = src.width,
       ySize = src.height,
       srcPixels = src.data,
-      dstPixels = dst.data,
       x, y, pos, dist;
 
+  var dst = new ImageData( xSize, ySize ),
+      dstPixels = dst.data;
   
   for(y=0; y<ySize; y++){
     for(x=0; x<xSize; x++){
       pos = (y*xSize+x)*4;
+
       dist = colorDist( color, [ srcPixels[pos+0], srcPixels[pos+1], srcPixels[pos+2] ] );
       if( dist > 255 ){ 
         dist = 255;
       }
+
       dstPixels[pos+0] = srcPixels[pos+0];
       dstPixels[pos+1] = srcPixels[pos+1];
       dstPixels[pos+2] = srcPixels[pos+2];
@@ -31,5 +34,7 @@ function colorToAlpha( src, dst, color ) {
 
     }
   }
+
+  return dst;
 
 }
