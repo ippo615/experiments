d645a246392a860f004663e5c285032d1db50e2a
Andrew Ippoliti <acippolit@gmail.com>
2014-08-29 22:16:24 -0400

    Build script is better organized and can perserve tests

--- js-sets/build.sh
+++ js-sets/build.sh
@@ -1,13 +1,13 @@
 files="
-	sets.js
-	contains.js
-	add-member.js
-	del-member.js
-	cardinality.js
-	equality.js
-	copy.js
-	union.js
-	merge.js
+	src/sets.js
+	src/contains.js
+	src/add-member.js
+	src/del-member.js
+	src/cardinality.js
+	src/equality.js
+	src/copy.js
+	src/union.js
+	src/merge.js
 "
 function lib(){
 	for f in $files; do
@@ -19,17 +19,26 @@ function lib(){
 	done;
 }
 
+function text(){
+	# Concatinate each file with a blank lin inbetween
+	for f in $files; do
+		cat "${f}"
+		echo
+	done;
+}
 
 COLOR_PASS='\E[0;32m'
 COLOR_WARN='\E[0;33m'
 COLOR_FAIL='\E[0;31m'
 
 
-function test(){
+function make_test(){
 	lib > "tmp/lib.js"
+	# Remove the old tests
+	rm -f tmp/test*
+	# Make tests for each source file
 	for f in $files; do
-		# Remov the old tests
-		rm -f tmp/test*
+		name=$(echo "$f" | sed -e 's|/|_|g' )
 		# For now the first part of the file is the implementation and
 		# the second part contains the tests/examples so
 		sed -n -e '/^##.*\(example\|propert\).*$/I,$p' "$f" |\
@@ -40,25 +49,32 @@ function test(){
 		uniq |\
 		# Create 1 file per test, each test is separated by a blank
 		# line. The files will be called tmp/test0, tmp/test1, ...
-		awk '{print $0 > "tmp/test" NR}' RS='\n\n'
+		awk "{print \$0 > \"tmp/test_$name\" NR}" RS='\n\n'
+	done;
+}
 
-		# Run every test and output if it passed/failed and any output
-		for t in tmp/test*; do
-			if [ -f "$t" ]; then
-				output=$(cat "tmp/lib.js" "$t" | nodejs 2>&1)
+function make_perf(){
+	make_test
+	# TODO: add performance tracking to test code
+}
 
-				if [[ $? -eq 0 ]]; then
-					echo -ne ${COLOR_PASS}
-					echo -ne "PASSED"
-				else
-					echo -ne ${COLOR_FAIL}
-					echo -ne "FAILED"
-				fi
-				tput sgr0
-				echo -ne " testing $t in $f"
-				echo " -- $output"
+function run_test(){
+	# Run every test and output if it passed/failed and any output
+	for t in tmp/test*; do
+		if [ -f "$t" ]; then
+			output=$(cat "tmp/lib.js" "$t" | nodejs 2>&1)
+
+			if [[ $? -eq 0 ]]; then
+				echo -ne ${COLOR_PASS}
+				echo -ne "PASSED"
+			else
+				echo -ne ${COLOR_FAIL}
+				echo -ne "FAILED"
 			fi
-		done;
+			tput sgr0
+			echo -ne " testing $t "
+			echo " -- $output"
+		fi
 	done;
 }
 
@@ -69,7 +85,9 @@ function clean(){
 	rm -r "tmp"
 }
 
-#lib
-setup 
-test
+setup
+#text > tmp/output.md
+#lib > tmp/lib.js
+make_test
+run_test
 clean
