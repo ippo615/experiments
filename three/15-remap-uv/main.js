9ba1f30e5b06ba8b26b1cd22c8ef46b1d071fee6
Andrew Ippoliti <acippolit@gmail.com>
2014-11-18 22:57:28 -0500

    Created spherical and cylindrical uv unwrappers

--- three/15-remap-uv/main.js
+++ three/15-remap-uv/main.js
@@ -10,6 +10,15 @@ function remap( value, fromMin, fromMax, toMin, toMax ){
 	return toMin + percent*dTo;
 }
 
+// For uv planar mapping along an arbitrary axis (or to an arbitrary
+// plane) I can create a rotation matrix and then apply it to the
+// geomtery before unwrapping. For example:
+// var mtx = new THREE.Matrix4().makeRotationX(45*Math.PI/180);
+// geomtery.applyMatrix(mtx);
+// uv_planar_xy(mtx);
+// var invMtx = new THREE.Matrix4().makeRotationX(45*Math.PI/180);
+// geomtery.applyMatrix(invMtx);
+
 function uv_planar_xy( geometry ){
 
 	// The min/max x,y will be the extemes (0,1) of our planar UV map
@@ -46,6 +55,103 @@ function uv_planar_xy( geometry ){
 	return geometry;
 }
 
+function uv_spherical( geometry ){
+
+	// We need the center of the geometer for this
+	// and the y extrema
+	geometry.computeBoundingBox();
+	var xMid = 0.5*(geometry.boundingBox.min.x+geometry.boundingBox.max.x);
+	var yMid = 0.5*(geometry.boundingBox.min.y+geometry.boundingBox.max.y);
+	var zMid = 0.5*(geometry.boundingBox.min.z+geometry.boundingBox.max.z);
+	var center = new THREE.Vector3( xMid, yMid, zMid );
+
+	// We use the vector from the point to the center to create 2 angles
+	// that describe the point as if it were projected onto a sphere
+	geometry.faceVertexUvs[0] = [];
+	for( var i=0,l=geometry.faces.length; i<l; i+=1 ){
+		var face = geometry.faces[i];
+
+		var ad = geometry.vertices[face.a].clone();
+		ad.sub( center );
+		ad.normalize();
+
+		var bd = geometry.vertices[face.b].clone();
+		bd.sub( center );
+		bd.normalize();
+
+		var cd = geometry.vertices[face.c].clone();
+		cd.sub( center );
+		cd.normalize();
+
+		geometry.faceVertexUvs[0].push([
+			new THREE.Vector2(
+				0.5+Math.atan2(ad.x,ad.z)/(2.0*Math.PI),
+				0.5-Math.asin(ad.y)/Math.PI
+			),
+			new THREE.Vector2(
+				0.5+Math.atan2(bd.x,bd.z)/(2.0*Math.PI),
+				0.5-Math.asin(bd.y)/Math.PI
+			),
+			new THREE.Vector2(
+				0.5+Math.atan2(cd.x,cd.z)/(2.0*Math.PI),
+				0.5-Math.asin(cd.y)/Math.PI
+			)
+		]);
+	}
+
+	geometry.uvsNeedUpdate = true;
+	return geometry;
+}
+
+function uv_cylindrical( geometry ){
+
+	// We need the center of the geometer for this
+	geometry.computeBoundingBox();
+	var xMid = 0.5*(geometry.boundingBox.min.x+geometry.boundingBox.max.x);
+	var yMid = 0.5*(geometry.boundingBox.min.y+geometry.boundingBox.max.y);
+	var zMid = 0.5*(geometry.boundingBox.min.z+geometry.boundingBox.max.z);
+	var center = new THREE.Vector3( xMid, yMid, zMid );
+	var yMin = geometry.boundingBox.min.y;
+	var yMax = geometry.boundingBox.max.y;
+
+	// We use the vector from the point to the center to create 2 angles
+	// that describe the point as if it were projected onto a sphere
+	geometry.faceVertexUvs[0] = [];
+	for( var i=0,l=geometry.faces.length; i<l; i+=1 ){
+		var face = geometry.faces[i];
+
+		var ad = geometry.vertices[face.a].clone();
+		ad.sub( center );
+		ad.normalize();
+
+		var bd = geometry.vertices[face.b].clone();
+		bd.sub( center );
+		bd.normalize();
+
+		var cd = geometry.vertices[face.c].clone();
+		cd.sub( center );
+		cd.normalize();
+
+		geometry.faceVertexUvs[0].push([
+			new THREE.Vector2(
+				0.5+Math.atan2(ad.x,ad.z)/(2.0*Math.PI),
+				remap( geometry.vertices[face.a].y, yMin, yMax, 0.0, 1.0 )
+			),
+			new THREE.Vector2(
+				0.5+Math.atan2(bd.x,bd.z)/(2.0*Math.PI),
+				remap( geometry.vertices[face.b].y, yMin, yMax, 0.0, 1.0 )
+			),
+			new THREE.Vector2(
+				0.5+Math.atan2(cd.x,cd.z)/(2.0*Math.PI),
+				remap( geometry.vertices[face.c].y, yMin, yMax, 0.0, 1.0 )
+			)
+		]);
+	}
+
+	geometry.uvsNeedUpdate = true;
+	return geometry;
+}
+
 // Create the scene
 var scene = new THREE.Scene();
 
@@ -61,7 +167,9 @@ document.body.appendChild( renderer.domElement );
 // Add a cube to the scene
 //var geometry = new THREE.BoxGeometry(3,3,3);
 var geometry = new THREE.SphereGeometry( 2, 12, 8 );
-geometry = uv_planar_xy( geometry );
+//geometry = uv_planar_xy( geometry );
+//geometry = uv_spherical( geometry );
+geometry = uv_cylindrical( geometry );
 var uvCanvas = THREE.UVsDebug(geometry,1024);
 uvCanvas.style.width = '50%';
 uvCanvas.style.position = 'absolute';
