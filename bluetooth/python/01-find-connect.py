b1692a0bbfae7f3ab75ca55bcf9c18b505a0390f
Andrew Ippoliti <acippolit@gmail.com>
2014-12-30 15:14:28 -0500

    Fixed bug(s) when looking for a specific device

--- bluetooth/python/01-find-connect.py
+++ bluetooth/python/01-find-connect.py
@@ -9,11 +9,10 @@ def find_device_address_by_name(name):
 	devices = bluetooth.discover_devices()
 
 	for devAddr in devices:
-		if deviceName == bluetooth.lookup_name( devAddr ):
+		if name == bluetooth.lookup_name( devAddr ):
 			return devAddr
 
-	if deviceAddress is None:
-		raise ValueError( 'Could not file device named: %s' % name )
+	raise ValueError( 'Could not file device named: %s' % name )
 
 deviceName = 'YP-G1'
 addr = find_device_address_by_name( deviceName )
