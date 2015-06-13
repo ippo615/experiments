923fa9b69684e551666bff0abf5bc85c7f04bd5c
Andrew Ippoliti <acippolit@gmail.com>
2015-06-13 01:14:24 -0400

    Example of capitalization by neural network with backprop

--- artificial-intelligence/neural-network/05-backprop.js
+++ artificial-intelligence/neural-network/05-backprop.js
@@ -188,37 +188,89 @@ var NeuralNet = (function(){
 	return NeuralNet;
 })();
 
-// ------------------------------------------------------------- Main -
-
-// Everything starts with random weights
-var aNet = new NeuralNet({
-	layers: [
-		new NeuronLayer({ nNeurons: 5 }),
-		new NeuronLayer({ nNeurons: 5 })
-	]
-});
-console.info( aNet.copy() );
-
-function TrainingData( input, output ){
-	this.input = input;
-	this.output = output;
+// --------------------------------------------------------------------
+//                           Example: Capitalization of ASCII Letters
+// --------------------------------------------------------------------
+function asciiToInput(ascii){
+	var val = ascii.charCodeAt(0);
+	return [
+		(val & 1<<0) === (1<<0) ? 1 : 0,
+		(val & 1<<1) === (1<<1) ? 1 : 0,
+		(val & 1<<2) === (1<<2) ? 1 : 0,
+		(val & 1<<3) === (1<<3) ? 1 : 0,
+		(val & 1<<4) === (1<<4) ? 1 : 0,
+		(val & 1<<5) === (1<<5) ? 1 : 0,
+		(val & 1<<6) === (1<<6) ? 1 : 0,
+		(val & 1<<7) === (1<<7) ? 1 : 0
+	];
+}
+function inputToAscii(value){
+	var code = 0;
+	code += (value[0] > 0.5) ?   1 : 0;
+	code += (value[1] > 0.5) ?   2 : 0;
+	code += (value[2] > 0.5) ?   4 : 0;
+	code += (value[3] > 0.5) ?   8 : 0;
+	code += (value[4] > 0.5) ?  16 : 0;
+	code += (value[5] > 0.5) ?  32 : 0;
+	code += (value[6] > 0.5) ?  64 : 0;
+	code += (value[7] > 0.5) ? 127 : 0;
+	return String.fromCharCode(code);
 }
 
-var trainingSet = [
-	new TrainingData( [0,1,1,1,1], [1,0,0,0,0] ),
-	new TrainingData( [1,0,1,1,1], [0,1,0,0,0] ),
-	new TrainingData( [1,1,0,1,1], [0,0,1,0,0] ),
-	new TrainingData( [1,1,1,0,1], [0,0,0,1,0] ),
-	new TrainingData( [1,1,1,1,0], [1,0,0,0,1] )
+var dataSet = [
+	new TrainingData( asciiToInput('a'), asciiToInput('A') ),
+	new TrainingData( asciiToInput('b'), asciiToInput('B') ),
+	new TrainingData( asciiToInput('c'), asciiToInput('C') ),
+	new TrainingData( asciiToInput('d'), asciiToInput('D') ),
+	new TrainingData( asciiToInput('e'), asciiToInput('E') ),
+	new TrainingData( asciiToInput('f'), asciiToInput('F') ),
+	new TrainingData( asciiToInput('g'), asciiToInput('G') ),
+	new TrainingData( asciiToInput('h'), asciiToInput('H') ),
+	new TrainingData( asciiToInput('i'), asciiToInput('I') ),
+	new TrainingData( asciiToInput('j'), asciiToInput('J') ),
+	new TrainingData( asciiToInput('k'), asciiToInput('K') ),
+	new TrainingData( asciiToInput('l'), asciiToInput('L') ),
+	new TrainingData( asciiToInput('m'), asciiToInput('M') ),
+	new TrainingData( asciiToInput('n'), asciiToInput('N') ),
+	new TrainingData( asciiToInput('o'), asciiToInput('O') ),
+	new TrainingData( asciiToInput('p'), asciiToInput('P') ),
+	new TrainingData( asciiToInput('q'), asciiToInput('Q') ),
+	new TrainingData( asciiToInput('r'), asciiToInput('R') ),
+	new TrainingData( asciiToInput('s'), asciiToInput('S') ),
+	new TrainingData( asciiToInput('t'), asciiToInput('T') ),
+	new TrainingData( asciiToInput('u'), asciiToInput('U') ),
+	new TrainingData( asciiToInput('v'), asciiToInput('V') ),
+	new TrainingData( asciiToInput('w'), asciiToInput('W') ),
+	new TrainingData( asciiToInput('x'), asciiToInput('X') ),
+	new TrainingData( asciiToInput('y'), asciiToInput('Y') ),
+	new TrainingData( asciiToInput('z'), asciiToInput('Z') )
 ];
 
+var capNet = new NeuralNet({
+	layers: [
+		new NeuronLayer({ nNeurons: 8 }),
+		new NeuronLayer({ nNeurons: 8 }),
+		new NeuronLayer({ nNeurons: 8 })
+	]
+});
+
+// Only train on some of the data, you'll see it works better when
+// trained on all of the data.
+// Also notice that the net is biased toward outputs it was taught.
+var trainingSampleCount = 26;
+
 for( var i=0; i<10000; i+=1 ){
-	for( var t=0, l=trainingSet.length; t<l; t+=1 ){
-		aNet.process( trainingSet[t].input );
-		aNet.learn( trainingSet[t].output, 2 );
+	for( var t=0, l=trainingSampleCount; t<l; t+=1 ){
+		capNet.process( dataSet[t].input );
+		capNet.learn( dataSet[t].output, 2 );
 	}
 }
 
-for( var t=0, l=trainingSet.length; t<l; t+=1 ){
-	console.info( aNet.process( trainingSet[t].input ) );
+for( var t=0, l=dataSet.length; t<l; t+=1 ){
+	console.info(
+		inputToAscii( dataSet[t].input )
+		+ ' -> '
+		+ inputToAscii( capNet.process( dataSet[t].input ) )
+	);
 }
+
