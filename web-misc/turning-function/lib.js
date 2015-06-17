060c9bb2d51bb824c1deee9e39478a3d83ff2223
Andrew Ippoliti <acippolit@gmail.com>
2015-06-17 00:20:06 -0400

    Added similarity score output to turning function

--- web-misc/turning-function/lib.js
+++ web-misc/turning-function/lib.js
@@ -52,6 +52,9 @@ var Polygon = (function(){
 			this.push( points[i].clone() );
 		}
 	};
+	Polygon.prototype.clone = function(){
+		return new Polygon( this.points );
+	};
 	Polygon.prototype.push = function( point ){
 		this.points.push( point.clone() );
 	};
@@ -113,6 +116,88 @@ var Polygon = (function(){
 			turnings.push( new Point( d, dir ) );
 		}
 		return turnings;
+	};
+
+	function distanceToSegment( point, start, end ){
+		// Vectors that represent the segment and point relationships
+		var vSegment = end.clone().sub( start );
+		var vPointStart = point.clone().sub( start );
+
+		// If segment is a point return the distance to that point
+		var segLength = vSegment.length();
+		if( segLength === 0 ){
+			return vPointStart.length();
+		}
+
+		// The percent between start and end the point is on the segment
+		var t = 0;
+		t += (vPointStart.x - vSegment.x)
+		t += (vPointStart.y - vSegment.y);
+		t /= segLength;
+
+		// If the percet is negative the point is closer to the start
+		if( t < 0 ){
+			return vPointStart.length()
+		}
+
+		// If the percent is above 1 the point is closer to the end
+		if( t > 1 ){
+			return point.clone().sub( end ).length();
+		}
+
+		// Otherwise the point is closer to a specific point in the middle
+		var nearest = new Point(
+			start.x + t*vSegment.x,
+			start.y + t*vSegment.y
+		);
+		return point.clone().sub( nearest ).length();
+	}
+
+	Polygon.prototype.getNearestDistance = function( point ){
+		var minDist = distanceToSegment( point, this.points[0], this.points[1] );
+		for( var i=1, l=this.points.length-1; i<l; i+=1 ){
+			var dist = distanceToSegment( point, this.points[i], this.points[i+1] );
+			if( dist < minDist ){
+				minDist = dist;
+			}
+		}
+		return minDist;
+	};
+	Polygon.prototype.similarity = function( other ){
+		var totalMinDist = 0;
+		for( var i=0, l=other.points.length; i<l; i+=1 ){
+			totalMinDist += this.getNearestDistance( other.points[i] );
+		}
+		return totalMinDist;
+	};
+	Polygon.prototype.matchAngle = function( other ){
+		// Create copies of the shapes so we can translate/rotate/scale
+		var cThis = this.clone();
+		var cThat = other.clone();
+
+		// Translate them so they have the same center
+		cThis.translate( cThis.center().scale( -1 ) );
+		cThat.translate( cThat.center().scale( -1 ) );
+
+		// Parameters (can be moved to function args)
+		var angleStart = 0;
+		var angleEnd = 360;
+		var angleStep = 5;
+
+		// 
+		var minSimilarity = cThis.similarity( cThat );
+		var minAngle = 0;
+
+		for( var angle = angleStart; angle < angleEnd; angle += angleStep ){
+			var rotThat = cThat.clone().rotateDeg( angle );
+			var similarity = cThis.similarity( rotThat );
+			if( similarity < minSimilarity ){
+				minAngle = angle;
+				minSimilarity = similarity;
+			}
+		}
+
+		return minAngle;
 	};
 
 	return Polygon;
