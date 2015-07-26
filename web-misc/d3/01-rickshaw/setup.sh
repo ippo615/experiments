1717f1958f80fefe4027cc4cb71ab623293f6fb7
Andrew Ippoliti <acippolit@gmail.com>
2015-07-26 10:46:47 -0400

    Created d3 and rickshaw real-time graphing example

---
+++ web-misc/d3/01-rickshaw/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scriptDir='vendor'
+mkdir ${scriptDir}
+#wget http://cdnjs.cloudflare.com/ajax/libs/rickshaw/1.5.1/rickshaw.min.js -O ${scriptDir}/rickshaw.min.js
+wget http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js -O ${scriptDir}/d3.min.js
+
