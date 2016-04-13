43b1ca09c0fecbef5d25af72a33eee6578f8266c
Andrew Ippoliti <acippolit@gmail.com>
2016-04-13 00:10:35 -0400

    More complex express examples

---
+++ express-js/05-generator/readme.md
@@ -0,0 +1,12 @@
+# Getting Started
+
+Once you cloned this you and want to get it running run:
+
+	cd /path/to/this/directory
+	npm install
+	mkdir data # for mongodb database
+
+Start the server:
+
+	npm start
+
---
+++ express-js/05-generator/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+module.exports = router;
